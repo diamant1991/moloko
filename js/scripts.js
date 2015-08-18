c911a2df6b736e0d634c868009998ec1c86ef041
diamant1991 <diamant1988@mail.ru>
2015-08-18 09:26:29 +0300

    add new files

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+  $("a.click").click(function () {
+    elementClick = $(this).attr("href")
+    destination = $(elementClick).offset().top;
+    $("html:not(:animated),body:not(:animated)").animate({scrollTop: destination}, 1100);
+    return false;
+  });
+  $(".hov_img").hover(
+     function () {
+         $(".three .right a").addClass("hov_img_effect");
+      },
+      function () {
+                $(".three .right a").removeClass("hov_img_effect");
+      }
+    );
+  var scroll = $("aside nav.service_content");
+   $(window).scroll(function(){
+  if ($(window).scrollTop()>=300) {
+       scroll.addClass('fix');
+  }
+  else scroll.removeClass('fix');
+ });
+
+   $('.phone').hover(function() {
+     $('header nav').animate({'margin-left': 0}, 'slow');
+     $('header nav li').animate({'margin-left': '3px', 'margin-right': '3px'}, 'slow')
+
+   }, function() {
+     $('header nav').animate({'margin-left': '10%'}, 'slow')
+     $('header nav li').animate({'margin-left': '20px', 'margin-right': '20px'}, 'slow')
+   });
+
+  
+
+});
